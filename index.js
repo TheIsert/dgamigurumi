aceaf23f82878ef9bb18a90c0bd0a368bd2ced27
TheIsert <francoaasemborn@gmail.com>
2022-02-17 13:50:38 -0300

    a

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app
 .set('view engine', 'ejs')
 .set("views", path.join(__dirname, "/views"))
 .use(express.urlencoded({extended: true}))
-app.listen(3000, () => {
-    console.log("Pagina prendida");
-});
+app.listen((process.env.PORT || 3000), () => {
+    console.log('pagina prendida');
+  });
