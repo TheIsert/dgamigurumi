aeda5584e37a1b2d6e30238824570ac7355af480
TheIsert <francoaasemborn@gmail.com>
2022-02-17 13:06:14 -0300

    web

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const path = require("path")
+const app = express()
+
+app
+.use("/", require("./routes/routes"))
+.use(express.static(__dirname + '/public'))
+.set('view engine', 'ejs')
+.set("views", path.join(__dirname, "/views"))
+.use(express.urlencoded({extended: true}))
+app.listen(3000, () => {
+    console.log("Pagina prendida");
+});
