24aa3259546b9ddb315ffaf1ed0c56de57fbb111
Franco Asemborn <francoaasemborn@gmail.com>
2022-07-08 17:46:08 -0300

    rework web

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require("express")
-const path = require("path")
-const app = express()
+const express = require("express");
+const path = require("path");
+const app = express();
 app
-.use("/", require("./routes/routes"))
-.use(express.static(__dirname + '/public'))
-.set('view engine', 'ejs')
-.set("views", path.join(__dirname, "/views"))
-.use(express.urlencoded({extended: true}))
-app.listen((process.env.PORT || 3000), () => {
-    console.log('pagina prendida');
-  });
+  .use("/", require("./routes/routes"))
+  .use(express.static(__dirname + "/public"))
+  .set("view engine", "ejs")
+  .set("views", path.join(__dirname, "/views"))
+  .use(express.urlencoded({ extended: true }));
+app.listen(process.env.PORT || 3000, () => {
+  console.log("pagina prendida");
+});
--- routes/routes.js
+++ routes/routes.js
@@ -2,8 +2,11 @@ const { Router } = require("express");
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.render("home");
-
+  res.render("home",{link:'localhost:3000/trabajos#pulpos'});
 });
+router.get("/trabajos", (req, res) => {
+  res.render("trabajos");
+});
+
 
 module.exports = router;
